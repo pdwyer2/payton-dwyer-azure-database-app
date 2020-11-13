0ea2214941c1146527aa3ceee73f6616f2708a45
Payton Dwyer <payton.dwyer38@gmail.comgit config --global user.email payton.dwyer38@gmail.comgit config --global user.name Paytonconfig --global payton.dwyer38@gmail.com>
2020-11-13 17:12:13 -0600

    updated package.json

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var conn = mysql.createConnection({
-    host: 'payton-dwyer-server-demo.mysql.database.azure.com',
+  host: 'payton-dwyer-server-demo.mysql.database.azure.com',
 	user: 'pdwyer@payton-dwyer-server-demo',
 	password: 'password1!',
-    database: 'demo',
+  database: 'demo',
 	port: 3306,
 	ssl: false
 }); 
